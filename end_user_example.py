4277bbca528ab75997cb526ba4d9631edb4c343c
Isaac Joseph <isaac.c.joseph@gmail.com>
2023-07-12 08:59:05 -0700

    renamed (#8)

--- end_user_example.py
+++
@@ -1,7 +0,0 @@
-import deprecator
-
-from library_module import replacement
-from library_module import previous_name
-
-
-print(previous_name())
--- test_deprecator.py
+++ test_deprecator.py
@@ -13,21 +13,21 @@ class TestDeprecator(unittest.TestCase):
         # 'foo' is not deprecated; ensure does not warn, as would throw exception
         with warnings.catch_warnings():
             warnings.simplefilter("error")
-            from library_module import replacement
+            from example_library_module import replacement
 
         # 'bar' is deprecated, warns
         with warnings.catch_warnings(record=True) as w:
-            from library_module import previous_name
+            from example_library_module import previous_name
 
             self.assertTrue(len(w) > 0)
             self.assertIn(
-                "'library_module.previous_name' has been renamed", str(w[0])
+                "'example_library_module.previous_name' has been renamed", str(w[0])
             )
             # old function still works, though:
             self.assertEqual("new function result", previous_name())
 
             # as does new function
-            from library_module import replacement
+            from example_library_module import replacement
 
             self.assertIn("new function result", replacement())
 
